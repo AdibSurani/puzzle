0fcc0fa02df8c9b936feff4ed20fc9d8c078fee3
adibsurani <adib.surani@gmail.com>
2014-03-03 22:01:27 +1100

    Initial commit
    Main window, and MUMS 2004 puzzle 5.3 Breakthrough

---
+++ MUMS/Breakthrough.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace puzzle.MUMS
+{
+    /// <summary>
+    /// Interaction logic for Breakthrough.xaml
+    /// </summary>
+    public partial class Breakthrough : Page
+    {
+        public Breakthrough()
+        {
+            InitializeComponent();
+            const string input = "tlg wVBtslAidcq oGrLDygoDok chzusR ONBpI nDoa jDk xsfdiAh bqI zfy kfDj wrmGxjc rd Hzu lhcv ga nmEBt sM skf oHvuIE pj ydv oGBAg snmidv ga nCjD iAtoadC ta gpbam Cqpbpgun hwe yql kFmGonbkjx hzkafcw rfadxoDp gtgpbgua tSEBtbqKsdr cqenBw";
+            MainTextBox.Text = Decode(input);
+            
+        }
+
+        static string Decode(string input)
+        {
+            var result = new StringBuilder();
+            int prevValue = 0;
+            foreach (var c in input)
+            {
+                if (c == ' ')
+                {
+                    result.Append(c);
+                }
+                else
+                {
+                    int nextValue = char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 1 + 26;
+                    result.Append((char)(Math.Abs(prevValue - nextValue) + 'A' - 1));
+                    prevValue = nextValue;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
